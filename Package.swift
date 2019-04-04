c6a8fa2736dee2805b38c929dcb641e46d21a2a0
Niklas Bülow <github@insightmind.net>
2019-04-04 10:20:30 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -2,29 +2,24 @@
 import PackageDescription
 
 let package = Package(
-	    name: "RealmSwift",
-	    products: [
-	        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-	        .library(
+	name: "RealmSwift",
+	products: [
+		// Products define the executables and libraries produced by a package, and make them visible to other packages.
+		.library(
 	            name: "Realm",
 	            targets: ["Realm"]),
 	        .library(
 	            name: "RealmSwift",
 	            targets: ["RealmSwift", "Realm"]),
-	    ],
-	    targets: [
-	        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-	        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-	        .target(
-	            name: "Realm",
-	            dependencies: []
-	        ),
+	],
+	targets: [
 	        .target(
 	            name: "RealmSwift",
 	            dependencies: ["Realm"],
+		    path: "RealmSwift"
 	            exclude: [
 	                "Tests",
 	            ]
-	        ),
-	    ]
+		),
+	]
 )
