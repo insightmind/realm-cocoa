690048ca7e2dbcc9897e08dd538d41caad484b1d
Niklas Bülow <github@insightmind.net>
2019-04-04 10:22:11 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,10 +4,6 @@ import PackageDescription
 let package = Package(
 	name: "RealmSwift",
 	products: [
-		// Products define the executables and libraries produced by a package, and make them visible to other packages.
-		.library(
-	            name: "Realm",
-	            targets: ["Realm"]),
 	        .library(
 	            name: "RealmSwift",
 	            targets: ["RealmSwift"]),
